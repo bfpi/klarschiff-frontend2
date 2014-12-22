612f6720a6a70215abd399c5f346a470af872ce4
Niels Bennke <bennke@bfpi.de>
2014-12-22 16:41:53 +0100

    Sicherstellen dass proj4 bereits definiert ist

--- javascripts/projections.js
+++ javascripts/projections.js
@@ -1,4 +1,6 @@
-proj4.defs('EPSG:25833', '+proj=utm +zone=33 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs');
-proj4.defs('urn:ogc:def:crs:EPSG::25833', proj4.defs('EPSG:25833'));
-var projection_25833 = ol.proj.get('EPSG:25833');
-projection_25833.setExtent(mv_bbox_25833);
+if (typeof proj4 === 'function') {
+  proj4.defs('EPSG:25833', '+proj=utm +zone=33 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs');
+  proj4.defs('urn:ogc:def:crs:EPSG::25833', proj4.defs('EPSG:25833'));
+  var projection_25833 = ol.proj.get('EPSG:25833');
+  projection_25833.setExtent(mv_bbox_25833);
+}
