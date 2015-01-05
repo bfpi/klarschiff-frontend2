8a4033fd9256fc33c25914218ad51e2e96652e2c
Niels Bennke <bennke@bfpi.de>
2015-01-05 16:15:16 +0100

    Browserweiche zur Erkennung von mobilen Endgeräten und event. Umleitungs auf das mobile Frontend

--- config/urls.php
+++ config/urls.php
@@ -5,6 +5,7 @@ $base_url = "http://klarschiff-test";
 define("ADRESSSUCHE_URL", $base_url . ":8080/solr/select?");
 define("BACKEND_URL", $base_url . "/backend/");
 define("FRONTEND_URL", $base_url . "/");
+define("MOBILE_FRONTEND_URL", $base_url . "/mobil");
 define("GEOSERVER_URL", $base_url . ":8080/geoserver/");
 define("MAP_URL", FRONTEND_URL . "map.php");
 define("WFS_URL", GEOSERVER_URL . "klarschiff/wms/reflect?layers=klarschiff:vorgaenge_rss&format=rss");
