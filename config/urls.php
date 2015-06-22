ac8c39f74560fd0bf4b4309923654599674ecb37
Niels Bennke <bennke@bfpi.de>
2015-06-22 09:12:11 +0200

    WFS-Urls einzeln konfigurierbar

--- config/urls.php
+++ config/urls.php
@@ -8,5 +8,6 @@ define("BACKEND_URL", BASE_URL . "backend/");
 define("FRONTEND_URL", BASE_URL . "pc/");
 define("MAP_URL", FRONTEND_URL . "map.php");
 define("MOBILE_FRONTEND_URL", BASE_URL . "mobil");
-define("OWS_URL", BASE_URL . "ows/klarschiff/ows");
-define("WFS_URL", BASE_URL . "ows/klarschiff/wms/reflect?layers=klarschiff:vorgaenge_rss&format=rss");
+define("MELDUNGEN_WFS_URL", BASE_URL . "ows/klarschiff/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=klarschiff_hgw:vorgaenge&outputFormat=application/json");
+define("STADTTEILE_WFS_URL", BASE_URL . "ows/klarschiff/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=klarschiff_hgw:klarschiff_stadtteile_hro&outputFormat=application/json");
+define("WFS_URL", BASE_URL . "ows/klarschiff/wms/reflect?layers=klarschiff_hgw:vorgaenge_rss&format=rss");
